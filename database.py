91134705449445a599ce20f55fa7991df2d71189
KuserOguzHan <hanoguz.mec.eng@gmail.com>
2024-09-29 14:57:34 +0100

    added tedst and prod_8

--- database.py
+++ database.py
@@ -2,23 +2,17 @@ import os
 from dotenv import load_dotenv
 from sqlmodel import create_engine, SQLModel, Session
 
-# Ortam değişkenine göre uygun .env dosyasını yükle
-env = os.getenv("ENV", "test")  # Varsayılan olarak "test"
-if env == "prod":
-    load_dotenv("prod.env")  # Prod ortamı için .env dosyasını yükle
-else:
-    load_dotenv("test.env")  # Test ortamı için .env dosyasını yükle
-
-# SQLALCHEMY_DATABASE_URL'u al
+load_dotenv()  # take environment variables from .env.
 SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+# print(SQLALCHEMY_DATABASE_URL)
 
-# Veritabanı motorunu oluştur
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
 
-# Veritabanı bilgilerini geri döndürmemiz gerekiyor kayıt için
+
+#Veritabanı bilgilerini geri döndürmemiz gerekiyor kayıt için.
 def get_db():
     db = Session(engine)
     try:
